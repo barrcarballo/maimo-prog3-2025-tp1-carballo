0edbf042c9b576a95343a9475e8ab5d96c49d089
barrcarballo <barbie.carb@gmail.com>
2025-05-15 21:40:59 -0300

    useState y useEffect

--- src/app/components/RecipesGrid.js
+++ src/app/components/RecipesGrid.js
@@ -1,14 +1,33 @@
+'use client'
+
+import { useState, useEffect } from "react";
 import RecipeCard from "@/app/components/RecipeCard";
 import data from "@/data/recipes";
+import axios from 'axios'
 
 const RecipesGrid = () => {
+const [data1, setData1] = useState([])
 
-  console.log(data)
+useEffect(()=> {
+
+  const getData = async () => {
+    const response = await axios.get(`https://dummyjson.com/recipes`)
+    const responseData = response.data.recipes
+    //console.log(responseData)
+    setData1(responseData)
+  }
+
+  getData()
+}, [])
 
   return (
     <div className="recipes_grid">
-      {data.recipes.map((recipe)=> ( // si el objeto no tiene id se pone index
-        <RecipeCard key={recipe.id} name={recipe.name} image={recipe.image}/>)
+      {data1.map(
+        (
+          recipe // si el objeto no tiene id se pone index
+        ) => (
+          <RecipeCard key={recipe.id} name={recipe.name} image={recipe.image} id={recipe.id}/>
+        )
       )}
     </div>
   );
